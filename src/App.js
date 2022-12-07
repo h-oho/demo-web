a2d6c69e5ba3b502af86e555f68b9121ab9f2e3d
h-oho <2ndsept@daum.net>
2022-12-08 00:08:58 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Wrapper from "./sample/Wrapper";
+import Hello from "./sample/Hello";
+import Counter from "./sample/Counter";
+import InputSample from "./sample/InputSample";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <InputSample />
+    )
 }
 
+
 export default App;
+
+
+/*import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App() {
+    const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
+}
+
+export default App;*/
---
+++ src/sample/Counter.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react";
+
+function Counter() {
+    const [count, setNumber] = useState(0);
+
+    const onIncrease = () => {
+        setNumber(prevNumber => prevNumber + 1);
+    }
+    const onDecrease = () => {
+        if(count > 0) {
+            setNumber(prevNumber => prevNumber - 1);
+        }
+    }
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
+        </div>
+    );
+}
+
+export default Counter;
